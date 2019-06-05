275490de04df02ec106ec37d8d36fc1dcaee993d
GabeDev <gabriel.d.albu@gmail.com>
2019-06-05 14:11:31 -0400

    new Screens

--- App.js
+++ App.js
@@ -1,74 +1,92 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {TextInput,Button, View, Text } from "react-native";
+import { createStackNavigator, createAppContainer } from "react-navigation";
 
-export default class App extends React.Component {
+class HomeScreen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-      <View style={styles.container}>
-
-      <View>
-      <Text>Username:</Text>
-      <TextInput/>
-      </View>
-
-        <View>
-        <Text>Password:</Text>
-        <TextInput secureTextEntry={true} style={{color: "black"}}/>
-        </View>
-
-        <View style={{flexDirection:'row'}}>
-            <Button
-            style={{padding:20}}
-              //onPress={onPressLearnMore}
-              title="Login"
-              color="#841584"
-              //accessibilityLabel="Learn more about this purple button"
-            />
-            <Button
-            //onPress={onPressLearnMore}
-            title="Register"
-            color="#841584"
-            //accessibilityLabel="Learn more about this purple button"
-            />
-        </View>
-
-        <View style={{flexDirection:'column'}}>
-        <Text>Sign in with</Text>
-            <Button
-            style={{padding:20}}
-              //onPress={onPressLearnMore}
-              title="Google"
-              color="#841584"
-              //accessibilityLabel="Learn more about this purple button"
-            />
-            <Button
-            //onPress={onPressLearnMore}
-            title="Facebook"
-            color="#841584"
-            //accessibilityLabel="Learn more about this purple button"
-            />
-
-            <Button
-            //onPress={onPressLearnMore}
-            title="LinkedIn"
-            color="#841584"
-            //accessibilityLabel="Learn more about this purple button"
-            />
-        </View>
-
-
-      </View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>Home Screen</Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+class Login extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <View style={styles.container}>
+
+    <View>
+    <Text>Username:</Text>
+    <TextInput/>
+    </View>
+
+      <View>
+      <Text>Password:</Text>
+      <TextInput secureTextEntry={true} style={{color: "black"}}/>
+      </View>
+
+      <View style={{flexDirection:'row'}}>
+          <Button
+          style={{padding:20}}
+            //onPress={onPressLearnMore}
+            title="Login"
+            color="#841584"
+
+          />
+          <Button
+          //onPress={onPressLearnMore}
+          title="Register"
+          color="#841584"
+
+          />
+      </View>
+
+      <View style={{flexDirection:'column'}}>
+      <Text>Sign in with</Text>
+          <Button
+          style={{padding:20}}
+            //onPress={onPressLearnMore}
+            title="Google"
+            color="#841584"
+
+          />
+          <Button
+          //onPress={onPressLearnMore}
+          title="Facebook"
+          color="#841584"
+
+          />
+
+          <Button
+          //onPress={onPressLearnMore}
+          title="LinkedIn"
+          color="#841584"
+
+          />
+      </View>
+
+
+    </View>
+      </View>
+    );
+  }
+}
+
+const AppNavigator = createStackNavigator({
+  Login: {
+    screen: Login
+  }
 });
+
+const styles = {
+
+container:
+{
+  
+},
+}
+
+export default createAppContainer(AppNavigator);
