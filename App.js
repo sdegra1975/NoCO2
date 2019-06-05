4e2ae754c1bdc651f31aa3d2a5bffaa8ec05de01
GabeDev <gabriel.d.albu@gmail.com>
2019-06-05 13:38:19 -0400

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,60 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+
+      <View>
+      <Text>Username:</Text>
+      <TextInput/>
+      </View>
+
+        <View>
+        <Text>Password:</Text>
+        <TextInput secureTextEntry={true} style={{color: "black"}}/>
+        </View>
+
+        <View style={{flexDirection:'row'}}>
+            <Button
+            style={{padding:20}}
+              //onPress={onPressLearnMore}
+              title="Login"
+              color="#841584"
+              //accessibilityLabel="Learn more about this purple button"
+            />
+            <Button
+            //onPress={onPressLearnMore}
+            title="Register"
+            color="#841584"
+            //accessibilityLabel="Learn more about this purple button"
+            />
+        </View>
+
+        <View style={{flexDirection:'column'}}>
+        <Text>Sign in with</Text>
+            <Button
+            style={{padding:20}}
+              //onPress={onPressLearnMore}
+              title="Google"
+              color="#841584"
+              //accessibilityLabel="Learn more about this purple button"
+            />
+            <Button
+            //onPress={onPressLearnMore}
+            title="Facebook"
+            color="#841584"
+            //accessibilityLabel="Learn more about this purple button"
+            />
+
+            <Button
+            //onPress={onPressLearnMore}
+            title="LinkedIn"
+            color="#841584"
+            //accessibilityLabel="Learn more about this purple button"
+            />
+        </View>
+
+
+      </View>
       </View>
     );
   }
